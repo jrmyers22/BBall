b9aeac3415c5f3dec228c13ecb353a16ee2fc802
Jacob Myers <jrmyers5@ncsu.edu>
2020-04-15 22:51:34 -0400

    Initial Commit

---
+++ BBall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBall
+//
+//  Created by Jacob Myers on 4/15/20.
+//  Copyright © 2020 Jacob Myers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
